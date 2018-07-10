92c264fe57497eec70f0f07f1e1a92890757c35c
HaapyDeng <jzhou_deng@163.com>
2018-07-10 10:19:24 +0800

    甲方演示版本

---
+++ app/src/main/java/com/max_plus/homedooropenplate/activity/killSelfService.java
@@ -0,0 +1,49 @@
+package com.max_plus.homedooropenplate.activity;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+
+/***
+ * 该服务只用来让APP重启，生命周期也仅仅是只是重启APP。重启完即自我杀死
+ */
+public class killSelfService extends Service {
+    /**
+     * 关闭应用后多久重新启动
+     */
+    private static long stopDelayed = 2000;
+    private Handler handler;
+    private String PackageName;
+
+    public killSelfService() {
+        handler = new Handler();
+    }
+
+    @Override
+    public int onStartCommand(final Intent intent, int flags, int startId) {
+        stopDelayed = intent.getLongExtra("Delayed", 2000);
+        PackageName = intent.getStringExtra("PackageName");
+        handler.postDelayed(new splashhandler(), stopDelayed);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    class splashhandler implements Runnable
+
+    {
+        public void run()
+
+        {
+            Intent LaunchIntent = getPackageManager().getLaunchIntentForPackage(PackageName);
+            startActivity(LaunchIntent);
+            killSelfService.this.stopSelf();
+        }
+    }
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+}
